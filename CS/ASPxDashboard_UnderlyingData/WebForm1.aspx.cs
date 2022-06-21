6f05f51590c56aba825705553c2c93f6d2468214
Natalia Kazakova <natalia.kazakova@devexpress.com>
2022-06-21 19:28:36 +0400

    update xml

--- CS/ASPxDashboard_UnderlyingData/WebForm1.aspx.cs
+++ CS/ASPxDashboard_UnderlyingData/WebForm1.aspx.cs
@@ -1,12 +1,6 @@
 ﻿using System;
 
-namespace ASPxDashboard_UnderlyingData
-{
-    public partial class WebForm1 : System.Web.UI.Page
-    {
-        protected void Page_Load(object sender, EventArgs e)
-        {
-            ASPxDashboard1.DashboardXmlPath = Server.MapPath("App_Data/dashboard1.xml");
-        }
+namespace ASPxDashboard_UnderlyingData {
+    public partial class WebForm1 : System.Web.UI.Page {
     }
 }
